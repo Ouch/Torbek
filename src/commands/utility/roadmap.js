fe11a47cdc5ee16972429e73a8da00437df3f84e
Tarla <Jeremybtx@icloud.com>
2024-09-14 18:25:52 -0500

    Update roadmap.js

--- src/commands/utility/roadmap.js
+++ src/commands/utility/roadmap.js
@@ -6,6 +6,6 @@ module.exports = {
   .setName('roadmap')
   .setDescription('Replies with a link or message of the To-Do list for Torbek.'),
   async execute(interaction) {
-      await interaction.reply(`Here's Torbeks project page! I'll have a link to his GitHub repository soon as well! \n https://github.com/users/Ouch/projects/3.`)
+      await interaction.reply(`Here's Torbeks project page! I'll have a link to his GitHub repository soon as well! \n https://github.com/Ouch/Torbek`)
     }
   }
